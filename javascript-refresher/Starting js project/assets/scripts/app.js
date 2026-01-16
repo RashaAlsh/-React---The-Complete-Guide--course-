183e0543f6e69d5228bbce6ebd1070dd16d8182d
rasha <rasha_alshraim@hotmail.com>
2026-01-16 10:15:11 +0100

    Add to app.js

--- javascript-refresher/Starting js project/assets/scripts/app.js
+++ javascript-refresher/Starting js project/assets/scripts/app.js
@@ -58,7 +58,7 @@ class User {
   user1.greet();
   
 
-//   write a new function that should be named combine
+// write a new function that should be named combine
 // Accept three input values , Calculate a new value
 // based on the three input values: a * b / c (if a, b & c are the input values)Return the calculated result
 
@@ -67,3 +67,30 @@ return a * b / c
 }
 
 console.log(combine(1 ,12 ,3))
+
+const hobbies = ["cooking","sport","reading"]
+console.log(hobbies[0])
+
+hobbies.push("Video game")
+
+console.log(hobbies)
+
+const sportIndex = hobbies.findIndex((item) => item === 'sport');
+
+console.log(sportIndex);
+
+const editedHobbies = hobbies.map((item) => item + "!")
+console.log(editedHobbies)
+
+const numberArray = [12, 15, 67];
+
+function transformToObjects(arr) {
+  return arr.map(num => ({ val: num }));
+}
+
+const result = transformToObjects(numberArray);
+console.log(result);
+
+const [firstName , lastName ] = ["Max","deiz"];
+console.log(firstName)
+console.log(lastName)
