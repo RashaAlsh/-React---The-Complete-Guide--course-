05dcbfac8ca28d29f1b3a2cf82b80fc7259e170e
rasha <rasha_alshraim@hotmail.com>
2026-01-23 10:44:01 +0100

    Splitting components

--- React-Essentials/first-project/src/App.jsx
+++ React-Essentials/first-project/src/App.jsx
@@ -1,53 +1,15 @@
-import { useState } from 'react';
-import {CORE_CONCEPTS} from './data'
 import Header from './components/Header/Header';
 import CoreConcepts from './components/CoreConcepts'
-import TabButton from './components/TabButton';
-import {EXAMPLES} from './data.js'
-
+import Examples from './components/Examples';
 
 function App() {
-const [selectedTopic , setSelectedTopic] = useState();
-
-  function handelSelect(selectedButton){
-   setSelectedTopic(selectedButton) 
-    console.log(selectedTopic)
-  }
-
-  console.log('App Component Executing')
 
   return (
     <> 
-       <Header/>
+     <Header/>
       <main>
-      <section id='core-concepts'>
-           <h2>Core Concepts</h2>
-           <ul>
-
-    {CORE_CONCEPTS.map((conceptItem)=> (<CoreConcepts key={conceptItem.title} {...conceptItem} ></CoreConcepts>))}
-
-           </ul>
-      </section>
-     <section id="examples">
-        <h2>Examples</h2>
-        <menu>
-          <TabButton isSelected={selectedTopic === "components"} onSelect={()=> handelSelect('components')}>Components</TabButton>
-          <TabButton isSelected={selectedTopic === "jsx"} onSelect={()=>handelSelect('jsx')}>JSX</TabButton>
-          <TabButton isSelected={selectedTopic === "props"} onSelect={()=>handelSelect('props')}>Props</TabButton>
-          <TabButton isSelected={selectedTopic === "state"} onSelect={()=>handelSelect('state')}>State</TabButton>
-        </menu>
-        {!selectedTopic ?( <p>Please Select a topic </p> ) : (
-        <div id='tab-content'> 
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>
-          {EXAMPLES[selectedTopic].code}
-          </code>
-        </pre>
-        </div>
-        )}
-     </section>
+    <CoreConcepts/> 
+    <Examples/>
       </main>
     </>
   );
